251795c439aee8af81dbb71bc1522c63621e89f0
ShiraShemesh <sh6706410@gmail.com>
2026-05-25 11:54:18 +0300

    add: Shira Shemesh joke and beautiful design

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,6 +5,8 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
+import ShiraCard from "./shira-shemesh/Card";
+import ShiraJokePage from "./shira-shemesh/JokePage";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
@@ -23,6 +25,13 @@ const students: Student[] = [
     jokeTitle: "התנין והתנינה",
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
+  },
+   {
+    id: "shira-shemesh",
+    name: "שירה שמש",
+    jokeTitle: "בננה נחמדת 🍌",
+    CardComponent: ShiraCard,
+    JokePageComponent: ShiraJokePage,
   },
 ];
 
