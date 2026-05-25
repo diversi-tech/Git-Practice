6d6bca27489b4381b20281899ae725b2cd45278c
YaelMalka <329467211@org.zzv.il>
2026-05-25 13:55:37 +0300

    add yael malka code

--- src/generated/branches.ts
+++ src/generated/branches.ts
@@ -2,12 +2,20 @@
 import type { BranchData } from "../types";
 
 const data: BranchData = {
-  "generatedAt": "2026-04-26T22:06:42.722Z",
-  "currentBranch": "",
+  "generatedAt": "2026-05-25T08:33:12.872Z",
+  "currentBranch": "master",
   "mainBranch": "master",
   "branches": [
     {
       "name": "master",
+      "parentBranch": null,
+      "shortHash": "0c10217",
+      "lastCommitMessage": "fix: instructions branch names",
+      "isMergedIntoParent": false,
+      "isCurrent": true
+    },
+    {
+      "name": "stars",
       "parentBranch": null,
       "shortHash": "",
       "lastCommitMessage": "",
@@ -15,157 +23,21 @@ const data: BranchData = {
       "isCurrent": false
     },
     {
-      "name": "diamond",
-      "parentBranch": "master",
-      "shortHash": "234",
-      "lastCommitMessage": "diamond",
+      "name": "stars-green",
+      "parentBranch": null,
+      "shortHash": "",
+      "lastCommitMessage": "",
       "isMergedIntoParent": false,
       "isCurrent": false
     },
     {
-      "name": "red",
-      "parentBranch": "diamond",
-      "shortHash": "23444",
-      "lastCommitMessage": "moshe",
+      "name": "stars-green-bat-sheva-bloch",
+      "parentBranch": null,
+      "shortHash": "",
+      "lastCommitMessage": "",
       "isMergedIntoParent": false,
       "isCurrent": false
-    },
-    {
-      "name": "miri",
-      "parentBranch": "red",
-      "shortHash": "23444",
-      "lastCommitMessage": "miri",
-      "isMergedIntoParent": false,
-      "isCurrent": false
-    },
-    {
-      "name": "roch",
-      "parentBranch": "red",
-      "shortHash": "23444",
-      "lastCommitMessage": "roch ",
-      "isMergedIntoParent": false,
-      "isCurrent": false
-    },
-    {
-      "name": "roch",
-      "parentBranch": "red",
-      "shortHash": "23444",
-      "lastCommitMessage": "roch ",
-      "isMergedIntoParent": false,
-      "isCurrent": false
-    },
-    {
-      "name": "avi nos",
-      "parentBranch": "red",
-      "shortHash": "23444",
-      "lastCommitMessage": "roch ",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "shia cjss  s",
-      "parentBranch": "red",
-      "shortHash": "23444",
-      "lastCommitMessage": "roch ",
-      "isMergedIntoParent": false,
-      "isCurrent": false
-    },
-    {
-      "name": "blue",
-      "parentBranch": "diamond",
-      "shortHash": "23444",
-      "lastCommitMessage": "moshe",
-      "isMergedIntoParent": false,
-      "isCurrent": false
-    },
-    {
-      "name": "red",
-      "parentBranch": "diamond",
-      "shortHash": "23444",
-      "lastCommitMessage": "moshe",
-      "isMergedIntoParent": false,
-      "isCurrent": true
-    },
-    {
-      "name": "star",
-      "parentBranch": "master",
-      "shortHash": "23454",
-      "lastCommitMessage": "yos",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "green",
-      "parentBranch": "star",
-      "shortHash": "23454",
-      "lastCommitMessage": "green",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "tut",
-      "parentBranch": "green",
-      "shortHash": "23454",
-      "lastCommitMessage": "green",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "brach",
-      "parentBranch": "green",
-      "shortHash": "23454",
-      "lastCommitMessage": "green",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "yochi",
-      "parentBranch": "green",
-      "shortHash": "23454",
-      "lastCommitMessage": "green",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "sjn cjknd",
-      "parentBranch": "green",
-      "shortHash": "23454",
-      "lastCommitMessage": "green",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "yellow",
-      "parentBranch": "star",
-      "shortHash": "23454",
-      "lastCommitMessage": "yellow fkgnlfg df",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "hjdf df",
-      "parentBranch": "yellow",
-      "shortHash": "23454",
-      "lastCommitMessage": "yellow fkgnlfg df",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "gfkj dfkn",
-      "parentBranch": "yellow",
-      "shortHash": "23454",
-      "lastCommitMessage": "yellow fkgnlfg df",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
-    {
-      "name": "fldngk  df",
-      "parentBranch": "yellow",
-      "shortHash": "23454",
-      "lastCommitMessage": "yellow fkgnlfg df",
-      "isMergedIntoParent": true,
-      "isCurrent": false
-    },
+    }
   ]
 };
 
--- src/students/index.ts
+++ src/students/index.ts
@@ -5,6 +5,8 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
+import YaelCard from "./yael-malka/Card";
+import YaelJokePage from "./yael-malka/JokePage";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
@@ -24,6 +26,13 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
+  {
+  id: "yael-malka",
+  name: "יעל מלכא",
+  jokeTitle: "מורה ותלמיד",
+  CardComponent: YaelCard,
+  JokePageComponent: YaelJokePage,
+},
 ];
 
 export default students;
