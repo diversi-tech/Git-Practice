c1cc711b73940c59c37f073114f38c01c6ac7afa
yaelirosner <yaeli.rosner@gmail.com>
2026-05-25 14:56:59 +0300

    add: הילה אריה בדיחה 2

--- src/students/index.ts
+++ src/students/index.ts
@@ -6,6 +6,9 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
 
+import HillaCard2 from "./Hilla-Arye-2/Card";
+import HillaJokePage2 from "./Hilla-Arye-2/JokePage";
+
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
 // אסור להשתמש בפונקציית-sort! 
@@ -24,6 +27,13 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
+  {
+  id: "Hilla-Arye-2",
+  name: "הילה אריה",
+  jokeTitle: "מנקה יסודי",
+  CardComponent: HillaCard2,
+  JokePageComponent: HillaJokePage2,
+},
 ];
 
 export default students;
