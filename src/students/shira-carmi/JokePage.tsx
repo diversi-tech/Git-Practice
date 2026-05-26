7269b05f7cd6bdc3978b03a0b1f6985b5f207906
shira carmi <shira.c215@gmail.com>
2026-05-26 12:59:21 +0300

    Update ShiraCarmiJokePage

--- src/students/shira-carmi/JokePage.tsx
+++ src/students/shira-carmi/JokePage.tsx
@@ -4,7 +4,7 @@ import { PageProps } from "../../types";
 export default function ShiraCarmiJokePage({ onBack }: PageProps) {
   return (
     <div className={`joke-page ${styles.jokePage}`}>
-      <p>למה מפתחים כל כך אוהבים להשתמש ב-Git?</p>
+      <p>למה מפתחים כל כךg אוהבים להשתמש ב-Git?</p>
       <p>כי זה המקום היחיד שבו מותר לדחוף (push) בלי לבקש סליחה!</p>
       <button onClick={onBack}>← Back</button>
     </div>
