cd489c6f02c255420ca471050bbfb6054f3e4b4d
Yehudit Vagshal <yehudit@example.com>
2026-05-26 12:02:48 +0300

    add: yehudit vagshal joke

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,6 +5,8 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
+import yehuditCard from "./yehudit vagshal/Card";
+import yehuditJokePage from "./yehudit vagshal/JokePage";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
@@ -23,6 +25,12 @@ const students: Student[] = [
     jokeTitle: "התנין והתנינה",
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
+  }, {
+    id: "yehudit-vagshal",
+    name: "יהודית וגשל",
+    jokeTitle: "השעון המעורר",
+    CardComponent: yehuditCard,
+    JokePageComponent: yehuditJokePage,
   },
 ];
 
