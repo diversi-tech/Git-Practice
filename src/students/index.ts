dd060d74aa67b798246d6f8289a5f55c9ac7c4ce
Tamar Damti <td0556730767@gmail.com>
2026-05-26 11:49:54 +0300

    add: tamar-damti joke

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,6 +5,8 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
+import TamarDamtiCard from "./tamar-damti/Card";
+import TamarDamtiJokePage from "./tamar-damti/JokePage";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
@@ -24,6 +26,13 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
+  {
+  id: "tamar-damti",
+  name: "תמר דמתי",
+  jokeTitle:"הבננה",
+  CardComponent: TamarDamtiCard,
+  JokePageComponent: TamarDamtiJokePage,
+},
 ];
 
 export default students;
