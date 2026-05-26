74984842e0176614d67be3f412395e6d00b73dcc
“etimunk <eti43357@gmail.com>
2026-05-26 12:42:08 +0300

    fix1

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,7 +5,7 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
-import EtiMunkCard from "./Eti-Munk/Card";
+import EtiMunkCard from "../Card";
 import EtiMunkJokePage from "./Eti-Munk/JokePage";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
