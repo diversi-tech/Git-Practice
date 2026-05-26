99fbf4b7f06b99be3644b02324d7034e5d56cd62
lital2005 <lital122105@gmail.com>
2026-05-26 14:24:53 +0300

    update index and components

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,6 +5,8 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
+import LitalShamanovJokePage from "./Lital_Shamanov/JokePage";
+import LitalShamanovCard from "./Lital_Shamanov/Card";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
@@ -24,6 +26,13 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
+  {
+  id: "lital-shamanov",
+  name: "ליטל שמנוב",
+  jokeTitle: "למה מתכנתים שונאים טבע?",
+  CardComponent: LitalShamanovCard,
+  JokePageComponent: LitalShamanovJokePage,
+},
 ];
 
 export default students;
