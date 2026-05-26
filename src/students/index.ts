2aee1bb67b5d607cf0ff5c8ba2b1ce78351acd3a
malka <m0556727693@gmail.com>
2026-05-26 11:26:20 +0300

    add malka joke

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,6 +5,8 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
+import MalkaCard from "./malka-gadlowitz/Card";
+import MalkaJokePage from "./malka-gadlowitz/JokePage";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
@@ -24,6 +26,13 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
+  {
+  id: "malka",
+  name: "מלכה גדלוביץ",
+  jokeTitle: "הבדיחה של מלכה",
+  CardComponent: MalkaCard,
+  JokePageComponent: MalkaJokePage,
+},
 ];
 
 export default students;
