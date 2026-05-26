584dd73d008d85974601bd26952a731987f57760
brc10 <b0556792776@gmail.com>
2026-05-26 12:57:33 +0300

    add brachas joke

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,11 +5,20 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
+import brachaJokePage from "./bracha-pappenheim/JokePage";
+import BrachaCard from "./bracha-pappenheim/Card";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
 // אסור להשתמש בפונקציית-sort! 
 const students: Student[] = [
+  {
+    id: "[bracha pappenheim]",
+    name: "[ברכה פפנהיים",
+    jokeTitle: "[שלום וברכה!",
+    CardComponent: BrachaCard,
+    JokePageComponent: brachaJokePage,
+  },
   {
     id: "dana-duvi",
     name: "דנה דובי",
@@ -24,6 +33,7 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
+
 ];
 
 export default students;
