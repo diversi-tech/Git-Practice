98fa60ee657e727a9a59de5ff927ec0a5e3e1599
מעיין בבחנוב <Maayan.nov.2006@gmail.com>
2026-05-26 12:45:48 +0300

    add: Maayan-Babahanov joke

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,23 +5,34 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
-import zehaviJokePage from "./zehavi-gerlitz/JokePage";
-import zehaviCard from "./zehavi-gerlitz/card";
+
 import EtiMunkCard from "./Eti-Munk/Card";
 import EtiMunkJokePage from "./Eti-Munk/JokePage";
 
+import MaayanCard from "./Maayan-Babahanov/Card";
+import MaayanJokePage from "./Maayan-Babahanov/JokePage";
+
+import zehaviJokePage from "./zehavi-gerlitz/JokePage";
+import zehaviCard from "./zehavi-gerlitz/card";
+
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
-// אסור להשתמש בפונקציית-sort! 
+// אסור להשתמש בפונקציית-sort!
 const students: Student[] = [
-
-    {
-  id: "zehavi-gerlitz",
-  name: "זהבי גרליץ",
-  jokeTitle: "כעכים או כאחים?",
-  CardComponent: zehaviCard,
-  JokePageComponent: zehaviJokePage,
-},
+  {
+    id: "Maayan-Babahanov",
+    name: "מעיין בבחנוב",
+    jokeTitle: "שיעור מחשבים 💻",
+    CardComponent: MaayanCard,
+    JokePageComponent: MaayanJokePage,
+  },
+  {
+    id: "zehavi-gerlitz",
+    name: "זהבי גרליץ",
+    jokeTitle: "כעכים או כאחים?",
+    CardComponent: zehaviCard,
+    JokePageComponent: zehaviJokePage,
+  },
   {
     id: "dana-duvi",
     name: "דנה דובי",
@@ -36,13 +47,12 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
-
   {
     id: "Eti Munk",
     name: "אתי מונק",
     jokeTitle: "לאכל עם כף ",
     CardComponent: EtiMunkCard,
-    JokePageComponent:EtiMunkJokePage,
+    JokePageComponent: EtiMunkJokePage,
   },
 ];
 
