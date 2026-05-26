405ce2a8616d2dd7be42b59457638b7fb2dce2a2
השם שלך <chanigerlitz@gmail.com>
2026-05-26 12:48:19 +0300

    add: [שמך] joke

--- src/students/index.ts
+++ src/students/index.ts
@@ -5,11 +5,21 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
+import zehaviJokePage from "./zehavi-gerlitz/JokePage";
+import zehaviCard from "./zehavi-gerlitz/card";
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
 // אסור להשתמש בפונקציית-sort! 
 const students: Student[] = [
+
+    {
+  id: "zehavi-gerlitz",
+  name: "זהבי גרליץ",
+  jokeTitle: "כעכים או כאחים?",
+  CardComponent: zehaviCard,
+  JokePageComponent: zehaviJokePage,
+},
   {
     id: "dana-duvi",
     name: "דנה דובי",
@@ -24,6 +34,7 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
+
 ];
 
 export default students;
