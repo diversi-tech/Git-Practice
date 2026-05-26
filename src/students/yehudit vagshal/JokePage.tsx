e9ac9b2111683947c235fef6f874f1b606ec3dc6
Yehudit Vagshal <yehudit@example.com>
2026-05-26 13:40:55 +0300

    הוספת בדיחה

--- src/students/yehudit vagshal/JokePage.tsx
+++ src/students/yehudit vagshal/JokePage.tsx
@@ -4,8 +4,8 @@ import { PageProps } from "../../types";
 export default function yehuditJokePage({ onBack }: PageProps) {
   return (
     <div className={`joke-page ${styles.jokePage}`}>
-      <p>joke setup</p>
-      <p>joke punchline</p>
+      <p>החלטתי לשים את השעון המעורר שלי בצד השני של החדר, כדי שאהיה חייבת לקום כדי לכבות אותו...</p>
+<p>עכשיו יש לי גם שמיכה וכרית בצד השני של החדר. 🛌</p>
       <button onClick={onBack}>← Back</button>
     </div>
   );
