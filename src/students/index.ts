e52443c36a578b791737490bc73fe162b7392c85
shanimishaely <sh8510077@gmail.com>
2026-05-26 22:12:37 +0300

    add: Shani Mishaely joke

--- src/students/index.ts
+++ src/students/index.ts
@@ -8,6 +8,8 @@ import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
 import RachelWeinbergerJokePage from "./rachel-weinberger/JokePage";
 import RachelWeinbergerCard from "./rachel-weinberger/Card";
 
+import ShaniCard from "./Shani-Mishaely/Card";
+import ShaniJokePage from "./Shani-Mishaely/JokePage";
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
 // אסור להשתמש בפונקציית-sort! 
@@ -26,12 +28,19 @@ const students: Student[] = [
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
-    {
+  {
     id: "rachel-weinberger",
     name: "רחלי וינברגר",
     jokeTitle: "כפית אחת יותר מדי",
     CardComponent: RachelWeinbergerCard,
     JokePageComponent: RachelWeinbergerJokePage,
+  },
+  {
+    id: "Shani-Mishaely",
+    name: "שני מישאלי",
+    jokeTitle: "סודות של קירות... 🧱",
+    CardComponent: ShaniCard,
+    JokePageComponent: ShaniJokePage,
   },
 ];
 
