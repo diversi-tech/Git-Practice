c0d746cfe2a9b979dac34d6aed2268dcfffb05f5
Yehudit Vagshal <yehudit@example.com>
2026-05-26 14:16:16 +0300

    commit number 1

--- src/students/yehudit vagshal/JokePage.tsx
+++ src/students/yehudit vagshal/JokePage.tsx
@@ -5,7 +5,7 @@ export default function yehuditJokePage({ onBack }: PageProps) {
   return (
     <div className={`joke-page ${styles.jokePage}`}>
       <p>החלטתי לשים את השעון המעורר שלי בצד השני של החדר, כדי שאהיה חייבת לקום כדי לכבות אותו...</p>
-<p>עכשיו יש לי גם שמיכה וכרית בצד השני של החדר. 🛌</p>
+<p>עכשיו יש לי גם שמיכה וכרית בצד השני של החד!!!ר. 🛌</p>
       <button onClick={onBack}>← Back</button>
     </div>
   );
