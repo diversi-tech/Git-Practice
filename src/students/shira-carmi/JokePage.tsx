d359d06cff3ccbf51af301e6f131942a50b5e008
shira carmi <shira.c215@gmail.com>
2026-05-26 13:45:20 +0300

    add emugy

--- src/students/shira-carmi/JokePage.tsx
+++ src/students/shira-carmi/JokePage.tsx
@@ -6,6 +6,7 @@ export default function ShiraCarmiJokePage({ onBack }: PageProps) {
     <div className={`joke-page ${styles.jokePage}`}>
       <p>למה מפתחים כל כךg אוהבים להשתמש ב-Git?</p>
       <p>כי זה המקום היחיד שבו מותר לדחוף (push) בלי לבקש סליחה!</p>
+      <p>😄</p>
       <button onClick={onBack}>← Back</button>
     </div>
   );
