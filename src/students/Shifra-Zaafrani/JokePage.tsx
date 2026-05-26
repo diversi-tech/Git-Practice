3fbfdaa065fa6e32a05a1c990d45232e7f1fd855
SHIFI-ZAFRANI <328518618@zzv.org.il>
2026-05-26 15:42:00 +0300

    fix: move punctuation marks inside string brackets

--- src/students/Shifra-Zaafrani/JokePage.tsx
+++ src/students/Shifra-Zaafrani/JokePage.tsx
@@ -6,10 +6,10 @@ export default function ShifraZaafraniJokePage({ onBack }: PageProps) {
 return (
         <div className={`joke-page ${styles.jokePage}`}>
             <p>{'איש אחד הולך ברחוב ורואה אדם יושב על המדרכה מחזיק שלט:'}</p>
-            <p>{'"אני קבצן , תנו לי כסף בבקשה."'}</p>
+            <p>{'"אני קבצן, תנו לי כסף בבקשה."'}</p>
             <p>{'האיש מתקרב אליו, נותן לו כסף ושואל אותו: "תגיד, למה אתה לא הולך לעבוד?"'}</p>
             <p>{'הקבצן עונה לו: "אני לא יכול לעבוד, אני קבצן!"'}</p>
             <button onClick={onBack}>Back</button>
         </div>
-    );
+    );git add .
 }
