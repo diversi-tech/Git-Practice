11367589c564307c2be76dd419e045409ad592c0
miriamBitan <bitan9505@gmail.com>
2026-05-27 20:55:19 +0300

    Revert "Catalyst iq 1"

--- src/students/index.ts
+++ src/students/index.ts
@@ -6,22 +6,10 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
 
-// מייבא את הקומפוננטות שלך
-import SheviShapiraCard from "./shevi-shapira/Card";
-import SheviShapiraJokePage from "./shevi-shapira/JokePage";
-import ChayaSprungJokePage from "./chaya-sprung/JokePage";
-import ChayaSprungCard from "./chaya-sprung/Card";
-
-import  efratMittelmanCard from "./efrat-mittelman/Card";
-import efratMittelmanJokePage from "./efrat-mittelman/JokePage";
-
-
-
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
 // אסור להשתמש בפונקציית-sort! 
 const students: Student[] = [
-
   {
     id: "dana-duvi",
     name: "דנה דובי",
@@ -30,35 +18,12 @@ const students: Student[] = [
     JokePageComponent: DanaDuviJokePage,
   },
   {
-  id: "chaya-sprung",
-  name: "חיה שפרונג",
-  jokeTitle: "בדיקת ראיה",
-  CardComponent: ChayaSprungCard,
-  JokePageComponent: ChayaSprungJokePage,
-},
-  {
     id: "chani-chanzin",
     name: "חני חנזין",
     jokeTitle: "התנין והתנינה",
     CardComponent: ChaniChanzinCard,
     JokePageComponent: ChaniChanzinJokePage,
   },
-  
-  
-    {
-    id: "efrat-mittelman",
-    name: "אפרת מיטלמן",
-    jokeTitle: "וירוס במחשב ",
-    CardComponent: efratMittelmanCard,
-    JokePageComponent: efratMittelmanJokePage,
-  },
-  {
-  id: "shevi-shapira",
-  name: "שבי שפירא",
-  jokeTitle: "אלגוריתם בסופרמרקט 🛒", // הכותרת החדשה והמתאימה
-  CardComponent: SheviShapiraCard,
-  JokePageComponent: SheviShapiraJokePage,
-},
 ];
 
 export default students;
