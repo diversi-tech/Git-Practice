7d2930f26ed0196d5cb10c3288d3fa1fe59b30e5
LNechami <nch39775@gmail.com>
2026-05-27 14:33:29 +0300

    add: nechami brim joke

--- src/students/nechami_brim/Card.tsx
+++ src/students/nechami_brim/Card.tsx
@@ -3,9 +3,18 @@ import { CardProps } from "../../types";
 
 export default function NechamiCard({ student, onClick }: CardProps) {
   return (
-    <div onClick={onClick} className={`joke-card ${styles.card}`}>
-      <h2 className={styles.title}>{student.jokeTitle}</h2>
-      <p className={styles.name}>{student.name}</p>
+<div onClick={onClick} className={`joke-card ${styles.card}`}>
+      <div style={{ fontSize: '2rem', marginBottom: '8px' }}>
+        👨‍💻✨
+      </div>
+
+      <h2 className={styles.title}>
+        {student.jokeTitle}
+      </h2>
+      
+      <div className={styles.name}>
+        <span>👤 {student.name}</span>
+      </div>
     </div>
   );
 }
