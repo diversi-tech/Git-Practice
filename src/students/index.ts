9c7fc841b7c2ad3e89025f5cea2049dae66415de
unknown <shugi5050@gmail.com>
2026-08-26 12:56:56 +0300

    fix: resolve merge conflicts

--- src/students/index.ts
+++ src/students/index.ts
@@ -6,32 +6,28 @@ import DanaDuviJokePage from "./dana-duvi/JokePage";
 import ChaniChanzinCard from "./chani-chanzin/Card";
 import ChaniChanzinJokePage from "./chani-chanzin/JokePage";
 
-<<<<<<< HEAD
 import AvishagCard from "./avishag/Card";
 import AvishagJokePage from "./avishag/jokePage";
-=======
 import ShilatBenGigiCard from "./shilat-ben-gigi/Card";
 import ShilatBenGigiJokePage from "./shilat-ben-gigi/JokePage";
->>>>>>> origin/stars-green-monabid
 
 // ⚠️ סדר הרשימה חייב להיות לפי סדר האלף-בית העברי לפי שם המשפחה.
 // כל תלמידה מוסיפה את עצמה במקום הנכון ידנית.
 // אסור להשתמש בפונקציית-sort! 
 const students: Student[] = [
   {
-<<<<<<< HEAD
     id: "avishag",
     name: "אבישג בן דוד",
     jokeTitle: "המחשב החולה",
     CardComponent: AvishagCard,
     JokePageComponent: AvishagJokePage,
-=======
+  },
+  {
     id: "shilat-ben-gigi",
     name: "שילת בן גיגי",
     jokeTitle: "הרמה הבאה",
     CardComponent: ShilatBenGigiCard,
     JokePageComponent: ShilatBenGigiJokePage,
->>>>>>> origin/stars-green-monabid
   },
   {
     id: "dana-duvi",
